81c60d0544e862de4f85a6f3511e12787c9ada0f
xvi <luefbfvehkdu@pascal.werz.name>
2022-12-03 09:17:47 +0100

    Initial Commit

---
+++ atenvc080/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  atenvc080
+//
+//  Created by Pascal on 03/12/2022.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
